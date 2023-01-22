1ae6b681889596a29767d9a251db0c894f1d65df
red23495 <mamun.rashid.hridoy@gmail.com>
2023-01-23 00:22:25 +0600

    voucher and ledger added

--- simple_accounting/accounting/admin.py
+++ simple_accounting/accounting/admin.py
@@ -1,5 +1,23 @@
-from django.contrib import admin
-from .models import Account
+from django.contrib import admin, messages
+from .models import Account, Voucher, VoucherType, Ledger
+from django.db import transaction
+from django.core.exceptions import ValidationError
+
+class AccountActiveListFilter(admin.SimpleListFilter):
+
+  title = 'Active Status'
+  parameter_name = 'active'
+
+  def lookups(self, request, model_admin):
+    return (
+      (1, 'Active'),
+      (0, 'Inactive')
+    )
+
+  def queryset(self, request, queryset):
+    if not self.value():
+      return queryset
+    return queryset.filter(inactive = self.value()=='0')
 
 class AccountAdmin(admin.ModelAdmin):
   fieldsets = [
@@ -9,8 +27,52 @@ class AccountAdmin(admin.ModelAdmin):
       }
     )
   ]
-  list_display = ('__str__', 'account_type', 'inactive')
+  list_display = ('__str__', 'account_type', 'is_active')
   list_display_links = ('__str__',)
   ordering = ('account_number',)
+  list_filter = ('account_type', AccountActiveListFilter,)
+  search_fields = ('name', 'account_number',)
+  autocomplete_fields=('parent',)
+  actions = ('make_active', 'make_inactive',)
+
+  def is_active(self, account):
+    return not account.inactive
+  
+  is_active.short_description = 'Active'
+  is_active.boolean = True
+
+  def set_inactive(self, request, queryset, value: bool):
+    with transaction.atomic():
+      models = queryset.order_by('account_number').all()
+      failed = 0
+      for model in models:
+        try:
+          model.inactive = value
+          model.full_clean()
+          model.save()
+        except ValidationError:
+          failed += 1
+      if failed:
+        self.message_user(request, f"failed to update {failed} accounts", level=messages.ERROR)
+
+  def make_active(self, request, queryset):
+    self.set_inactive(request, queryset, False)
+
+  def make_inactive(self, request, queryset):
+    self.set_inactive(request, queryset, True)
+
+  make_active.short_description = 'Activate'
+  make_inactive.short_description = 'Deactivate'
+
+
+class LedgerInline(admin.TabularInline):
+  model = Ledger
+
+class VoucherAdmin(admin.ModelAdmin):
+  list_display = ('__str__', 'voucher_date', 'amount')
+  ordering = ('voucher_number',)
+  inlines = [LedgerInline]
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(VoucherType)
+admin.site.register(Voucher, VoucherAdmin)
--- simple_accounting/accounting/models.py
+++ simple_accounting/accounting/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, date
 from django.db import transaction
 from django.core.exceptions import ValidationError
+from sequences import get_next_value
+from decimal import Decimal
+
+def non_empty_validator(value: str):
+  if len(value.strip()) == 0:
+      raise ValidationError('This field can not be empty')
+
+def non_zero_validator(value):
+  if value == 0:
+    raise ValidationError('This field can not be 0')
 
 class Account(models.Model):
 
@@ -12,8 +22,8 @@ class Account(models.Model):
     REVENUE = 4
     EXPENSE = 5
 
-  name: str = models.CharField(max_length=256, blank=False)
-  account_number: str = models.CharField(max_length=32, unique=True, blank=False, db_index=True)
+  name: str = models.CharField(max_length=256, blank=False, validators=[non_empty_validator])
+  account_number: str = models.CharField(max_length=32, unique=True, blank=False, db_index=True, validators=[non_empty_validator])
   account_type: AccountTypes = models.IntegerField(choices=AccountTypes.choices)
   parent: "Account" = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
   description: str = models.TextField(blank=True)
@@ -30,12 +40,6 @@ class Account(models.Model):
 
   def clean(self):
     errors = {}
-    if not len(self.name.strip()) > 0:
-      errors['name'] = "name can not be blank"
-    if not len(self.account_number.strip()) > 0: 
-      errors['account_number'] = "account number can not be blank"
-    if self.account_type is None: 
-      errors['account_type'] = "account type can not be empty"
     if not (self.parent is None or self._state.adding == False or self.parent.inactive == False):
       errors['parent'] = "can't create child for inactive parent"
     if self.parent and not self.account_number.startswith(f'{self.parent.account_number}.'):
@@ -56,3 +60,101 @@ class Account(models.Model):
 
   def __str__(self):
     return f"{self.account_number} - {self.name}"
+
+
+class VoucherType(models.Model):
+  
+  name: str = models.CharField(max_length=128, blank=False, validators=[non_empty_validator])
+  prefix: str = models.CharField(max_length=4, unique=True, blank=False, validators=[non_empty_validator])
+
+  def generate_number(self):
+    return f'{self.prefix}-{str(get_next_value(self.prefix)).zfill(4)}'
+
+  def __str__(self):
+    return f'{self.name} ({self.prefix})'
+
+class Voucher(models.Model):
+
+  class Status(models.IntegerChoices):
+    PENDING = 1
+    APPROVED = 2
+    REJECTED = 3
+
+  voucher_number: str = models.CharField(max_length=12, editable=False)
+  voucher_date: date = models.DateField()
+  voucher_type: VoucherType = models.ForeignKey(VoucherType, on_delete=models.CASCADE, blank=False, null=False)
+  description: str = models.TextField(null=True, blank=True)
+  status: Status = models.IntegerField(choices=Status.choices, blank=False, default=Status.PENDING)
+  created_at: datetime = models.DateTimeField(auto_now_add=True)
+  updated_at: datetime = models.DateTimeField(auto_now=True)
+
+  _status_changed = False
+
+  def __setattr__(self, __name: str, __value: any) -> None:
+    if hasattr(self, '_state') and not self._state.adding and __name == 'status' and __value != self.status:
+      self._status_changed = True
+    return super().__setattr__(__name, __value)
+
+  def set_ledgers(self, ledgers):
+    with transaction.atomic():
+      for ledger in ledgers:
+        if isinstance(ledger, dict):
+          ledger = Ledger(**ledger)
+        ledger.voucher = self
+        ledger.status = self.status
+        ledger.full_clean()
+        ledger.save()
+      debit_amount = sum(item.amount for item in self.debits)
+      credit_amount = sum(item.amount for item in self.credits)
+      if debit_amount != credit_amount:
+        raise ValidationError('Debit Credit must be equal')
+
+  @property
+  def debits(self):
+    ledgers = self.ledgers.all()
+    debits = []
+    for ledger in ledgers:
+      if ledger.amount > 0 and ledger.account.account_type in [Account.AccountTypes.ASSET, Account.AccountTypes.EXPENSE]:
+        debits.append(ledger)
+      elif ledger.amount < 0 and ledger.account.account_type in [Account.AccountTypes.LIABILITY, Account.AccountTypes.EQUITY, Account.AccountTypes.REVENUE]:
+        debits.append(ledger)
+    return debits
+
+  @property
+  def credits(self):
+    ledgers = self.ledgers.all()
+    credits = []
+    for ledger in ledgers:
+      if ledger.amount < 0 and ledger.account.account_type in [Account.AccountTypes.ASSET, Account.AccountTypes.EXPENSE]:
+        credits.append(ledger)
+      elif ledger.amount > 0 and ledger.account.account_type in [Account.AccountTypes.LIABILITY, Account.AccountTypes.EQUITY, Account.AccountTypes.REVENUE]:
+        credits.append(ledger)
+    return credits
+
+  @property
+  def amount(self):
+    return sum(item.amount for item in self.debits)
+
+  def save(self, **kwargs):
+    with transaction.atomic():
+      if self._state.adding:
+        self.voucher_number = self.voucher_type.generate_number()
+      super(Voucher, self).save(**kwargs)
+      if self._status_changed:
+        Ledger.objects.filter(voucher=self).update(status=self.status)
+        self._status_changed = False
+
+  def __str__(self):
+    return self.voucher_number
+
+class Ledger(models.Model):
+
+  voucher: Voucher = models.ForeignKey(Voucher, on_delete=models.CASCADE, null=False, blank=False, related_name='ledgers')
+  account: Account = models.ForeignKey(Account, on_delete=models.CASCADE, null=False, blank=False, related_name='+')
+  amount: Decimal = models.DecimalField(max_digits=30, decimal_places=6, validators=[non_zero_validator])
+  status: Voucher.Status = models.IntegerField(choices=Voucher.Status.choices)
+  created_at: datetime = models.DateTimeField(auto_now_add=True)
+  updated_at: datetime = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'{self.voucher.voucher_number} - {self.account.name}'
